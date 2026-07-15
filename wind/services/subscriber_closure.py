aedc83c8f99fa9799a22e407c3633b0db499c9a4
leompe8907 <veyqpasa1312@gmail.com>
2026-07-15 17:17:35 -0300

    Fix typo in user deactivation function call in close_subscriber_account: removed redundant 's' at the end of the function name for correct execution.

--- wind/services/subscriber_closure.py
+++ wind/services/subscriber_closure.py
@@ -165,7 +165,7 @@ def close_subscriber_account(
         )
 
     local_result["registry"] = _mark_registry_closed(subscriber_code, closed_at)
-    local_result["users_deactivated"] = _deactivate_portal_users(subscriber_code)
+    local_result["users_deactivated"] = _deactivate_portal_users(subscriber_code)s(subscriber_code)
     local_result["udid_revoked"] = _revoke_udid_requests(subscriber_code)
 
     log_status = SubscriberClosureLog.STATUS_COMPLETED
